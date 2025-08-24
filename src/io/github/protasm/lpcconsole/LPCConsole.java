01d39f88e4824d9ef01ba9d069bd11eca7021519
Jurisware Developer <devel@jurisware.com>
2025-08-24 14:23:46 -0400

    devel/refactor

--- src/io/github/protasm/lpcconsole/LPCConsole.java
+++ src/io/github/protasm/lpcconsole/LPCConsole.java
@@ -2,6 +2,6 @@ package io.github.protasm.lpcconsole;
 
 public class LPCConsole {
 	public static void main(String[] args) {
-		System.out.println("Hello again, world!");
+		System.out.println("Hello again, whole world!");
 	}
 }
